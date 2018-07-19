8e7e170c4642c05c19053ac53e9732fff8c45271
Mohan Singh <mslogicmaster@gmail.com>
2018-07-19 19:29:32 +0530

    bdr

---
+++ src/app/core/data/smart-table.model.ts
@@ -0,0 +1,10 @@
+export interface  SmartTableModel {
+    call_from: 'string',
+    call_to: 'string',
+    call_type: 'string',
+    bill_hrsmin: 'string',
+    billmin: 'string',
+	start_time: 'string',
+	call_from_db: 'string',
+	call_to_db: 'string'
+}
---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,23 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Grid',
+    icon: 'nb-tables',
+	link: '/pages/tables/smart-table',
+    home: true
+  },
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+  },{
+    title: 'Message',
+    icon: 'nb-compose',
+  },{
+    title: 'Settings',
+    icon: 'nb-gear',
+  },{
+    title: 'Location',
+    icon: 'nb-location',
+  },
+];
